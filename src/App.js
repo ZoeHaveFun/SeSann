23986c549ff6067acee92d12f608f36d1d644840
ZoeFan1995 <unique84060327@gmail.com>
2022-06-15 14:06:55 +0800

    feat: database test

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
 import './App.css';
-// import db from './firestore';
+import { doc, getDoc } from 'firebase/firestore';
+import { useEffect } from 'react';
+import db from './firestore';
 
 function App() {
+  async function getData() {
+    const docRef = doc(db, 'Market', 'Ub5pVDOCDn0qs9ZfYjff');
+    const docSnap = await getDoc(docRef);
+    console.log(docSnap.data());
+  }
+  useEffect(() => { getData(); }, []);
+
   return (
     <div className="App">很棒</div>
   );
