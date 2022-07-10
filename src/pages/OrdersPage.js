a7c308d7e79acf91f70341bad563af61e48b42bb
ZoeFan1995 <unique84060327@gmail.com>
2022-07-11 00:02:31 +0800

    feat: complete oreders list layout

--- src/pages/OrdersPage.js
+++ src/pages/OrdersPage.js
@@ -1,15 +1,22 @@
 import { useContext } from 'react';
+import styled from 'styled-components';
 import { firebaseUsers } from '../utils/firestore';
 import { OrderList } from '../components/List';
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 12px 0px;
+`;
 
 function OrdersPage() {
   const userInfo = useContext(firebaseUsers.AuthContext);
   return (
-    <div>
+    <Wrapper>
       {
          userInfo.orders?.map?.((item) => <OrderList item={item} key={item.user_id} />)
       }
-    </div>
+    </Wrapper>
   );
 }
 export default OrdersPage;
