cd1d90df588591ac764e366e90dcb4b1cec12c47
ZoeFan1995 <unique84060327@gmail.com>
2022-07-16 15:42:27 +0800

    feat: add user record card

--- src/pages/InformationPage.js
+++ src/pages/InformationPage.js
@@ -95,7 +95,7 @@ const ReserveDetail = styled.div`
 const ButtonWrapper = styled.div`
   display: flex;
   flex-direction: column;
-  justify-content: space-around;
+  justify-content: ${(props) => (props.isRecord ? 'flex-end' : 'space-around')} ;
 `;
 const Button = styled.button`
   padding: 8px 10px;
@@ -201,15 +201,139 @@ RemindCard.propTypes = {
   CancelReserve: PropTypes.func.isRequired,
 };
 
+const RecordWrapper = styled.div`
+  display: flex;
+  padding: 10px 20px;
+  border-radius: 0.8rem;
+  box-shadow: 0px 0px 4px 1px #DDE1E4;
+  border: ${(props) => (props.isExpiredReserve ? '2px #b64a41 solid' : '2px #a5be00 solid')};
+  &+&{
+    margin-bottom: 10px;
+  }
+`;
+const Titile = styled.h3`
+  color: #DDE1E4;
+  font-weight: 500;
+  > span {
+    margin-right: 8px;
+    color: ${(props) => (props.isExpiredReserve ? '#b64a41' : '#a5be00')};
+  }
+`;
+const IknowButton = styled.button`
+  padding: 4px 12px;
+  background-color: #E7ECEF;
+  color: #8B8C89;
+  box-shadow: 0px 0px 4px #8B8C89;
+  font-family: 'Noto Sans TC', sans-serif;
+  cursor: pointer;
+  border-radius: 0.8rem;
+  &:hover {
+    background-color: #8B8C89;
+    color: #E7ECEF;
+  }
+`;
+function RecordCard({ record, deletRecord }) {
+  return (
+    <RecordWrapper isExpiredReserve={record.reserve_id}>
+      <Link to={`/store?store_id=${record.store_id}?machine_id=${record.machine_id}`}>
+        <StorePart>
+          <div>
+            <StoreImg src={DefaultstoreMainImg} />
+            <span>{record.store_name}</span>
+          </div>
+          <span>
+            {
+              record.reserve_time
+                ? `${dayjs(record.estimate_startTime.seconds * 1000).format('YYYY/MM/DD HH:mm:ss')}`
+                : `${dayjs(record.start_time.seconds * 1000).format('YYYY/MM/DD HH:mm:ss')}`
+            }
+          </span>
+        </StorePart>
+      </Link>
+      <Container>
+        <Left>
+          {record.process_id
+            ? (
+              <Titile>
+                <span>IT&apos;S DONE!</span>
+                你的洗滌項目:
+              </Titile>
+            )
+            : (
+              <Titile isExpiredReserve>
+                <span>IT&apos;S CANCEL!</span>
+                你的預約項目已逾期:
+              </Titile>
+            )}
+          <ReserveDetail>
+            <div>
+              <span>
+                <AccessTime />
+                {record.category.time}
+              </span>
+              <span>
+                <MonetizationOn />
+                {record.category.price}
+              </span>
+            </div>
+            <span>{record.category.name}</span>
+            <span>{record.machine_name}</span>
+          </ReserveDetail>
+        </Left>
+      </Container>
+      <ButtonWrapper isRecord>
+        <IknowButton onClick={() => { deletRecord(record.process_id || record.reserve_id); }}>
+          我知道了
+        </IknowButton>
+      </ButtonWrapper>
+    </RecordWrapper>
+  );
+}
+RecordCard.propTypes = {
+  record: PropTypes.shape({
+    reserve_id: PropTypes.string,
+    process_id: PropTypes.string,
+    machine_id: PropTypes.string.isRequired,
+    machine_name: PropTypes.string.isRequired,
+    store_id: PropTypes.string.isRequired,
+    store_name: PropTypes.string.isRequired,
+    reserve_time: PropTypes.shape({
+      seconds: PropTypes.number.isRequired,
+    }),
+    estimate_startTime: PropTypes.shape({
+      seconds: PropTypes.number.isRequired,
+    }),
+    start_time: PropTypes.shape({
+      seconds: PropTypes.number.isRequired,
+    }),
+    category: PropTypes.shape({
+      name: PropTypes.string.isRequired,
+      price: PropTypes.number.isRequired,
+      time: PropTypes.number.isRequired,
+    }).isRequired,
+  }).isRequired,
+  deletRecord: PropTypes.func.isRequired,
+};
+
 const Wrapper = styled.div`
   display: flex;
   flex-direction: column;
   gap: 12px 0px;
 `;
+const Message = styled.h3`
+  width: 100%;
+  box-shadow: 0px 0px 8px #e7ecef;
+  border-radius: 0.8rem;
+  padding: 20px 0px;
+  color: #bec5c9;
+  text-align: center;
+  font-size: 18px;
+`;
 function InformationPage() {
   const userInfo = useContext(firebaseUsers.AuthContext);
   const userId = userInfo.user_id;
   const [reminds, setReminds] = useState([]);
+  const [records, setRecords] = useState([]);
 
   const CancelReserve = async (machineId, reserveId) => {
     const reserveMachineData = await firebaseMachines.getOne(machineId);
@@ -220,6 +344,16 @@ function InformationPage() {
     firebaseReserve.delet(reserveId);
     setReminds(newRemids);
   };
+  const deletRecord = (id) => {
+    const data = [...records];
+    const newRecords = data.filter((list) => list.reserve_id !== id && list.process_id !== id);
+    setRecords(newRecords);
+    firebaseUsers.updateRecords(userId, newRecords);
+  };
+
+  useEffect(() => {
+    setRecords(userInfo.records);
+  }, [userInfo]);
   useEffect(() => {
     firebaseReserve.getQuery(userId, 'user_id')
       .then((res) => res.map((item) => item.data()))
@@ -236,13 +370,44 @@ function InformationPage() {
   return (
     <Wrapper>
       {
-        reminds?.map?.((remind) => (
-          <RemindCard
-            remind={remind}
-            key={remind.reserve_id}
-            CancelReserve={CancelReserve}
-          />
-        ))
+        reminds.length === 0 && records.length === 0 ? (
+          <Message>
+            “你可以帮我洗個東西嗎”
+            <br />
+            “洗什麼”
+            <br />
+            “喜歡我”
+          </Message>
+        )
+          : (
+            reminds?.map?.((remind) => (
+              <RemindCard
+                remind={remind}
+                key={remind.reserve_id}
+                CancelReserve={CancelReserve}
+              />
+            ))
+          )
+      }
+      {
+        reminds.length === 0 && records.length === 0 ? (
+          <Message>
+            “你可以帮我洗個東西嗎”
+            <br />
+            “洗什麼”
+            <br />
+            “喜歡我”
+          </Message>
+        )
+          : (
+            records?.map?.((record) => (
+              <RecordCard
+                record={record}
+                key={record.reserve_id || record.process_id}
+                deletRecord={deletRecord}
+              />
+            ))
+          )
       }
     </Wrapper>
   );
