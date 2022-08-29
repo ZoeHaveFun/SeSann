70e69a50bfc2793356f9d1dd5567518834377339
ZoeFan1995 <unique84060327@gmail.com>
2022-08-29 14:40:40 +0800

    fix: add error message and onKeyup event

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,4 +1,4 @@
-import { useRef, useState } from 'react';
+import { useRef } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import styled from 'styled-components/macro';
 import { firebaseUsers } from '../utils/firestore';
@@ -39,51 +39,91 @@ const LoginWrapper = styled.div`
   font-family: 'Noto Sans TC', sans-serif;
 `;
 const LeftBanner = styled.div`
-  /* background-color: ; */
-  padding: 30px 10px 30px 40px;
+  padding: 30px 20px;
 `;
 const RightForm = styled.div`
   margin-left: 8px;
   border-left: 0.1px #0e6ba8 solid;
   padding: 30px 40px 30px 30px;
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
   & > h2 {
     text-align: center;
     letter-spacing: 0.2rem;
     font-weight: 500;
     color: #001c55;
   }
-  & > label {
-    padding: 10px 0px;
+  & > form {
+    label {
+      color: #001c55;
+      font-family: 'Noto Sans TC', sans-serif;
+      display: flex;
+      flex-direction: column;
+      margin-bottom: 10px;
+      font-size: 16px;
+      span {
+        padding: 4px 6px;
+      }
+      input {
+        font-size: 16px;
+        background-color: none;
+        padding: 4px 6px;
+        border-radius: 0.2rem;
+        border: transparent;
+        border-bottom: 1px #DDE1E4 solid;
+        &:-webkit-autofill,
+        &:-webkit-autofill:hover,
+        &:-webkit-autofill:focus,
+        &:-webkit-autofill:active {
+          -webkit-transition: "color 9999s ease-out, background-color 9999s ease-out";
+          -webkit-transition-delay: 9999s;
+        }
+      }
+    }
   }
-  & > button {
-    background-color: #001c55;
-    padding: 4px 0px;
-    border-radius: 0.8rem;
-    color: #ffffff;
-    font-family: 'Noto Sans TC', sans-serif;
-    cursor: pointer;
-    font-size: 16px;
-    margin-top: 10px;
-    &:hover {
-      background-color: #ff9f1c;
+  button {
+      width: 100%;
+      background-color: #001c55;
+      padding: 4px 0px;
+      border-radius: 0.8rem;
+      color: #ffffff;
+      font-family: 'Noto Sans TC', sans-serif;
+      cursor: pointer;
+      font-size: 16px;
+      margin-top: 10px;
+      &:hover {
+        background-color: #ff9f1c;
+      }
+    }
+  p {
+    margin-top: 8px;
+    font-size: 14px;
+    text-align: end;
+    > a {
+      color: #0e6ba8;
+      &:hover {
+        color: #ff9f1c;
+      }
     }
   }
 `;
 
 function Login() {
-  const [errorMessage, setErrorMessage] = useState('');
-  const loginEmail = useRef();
-  const loginPassword = useRef();
+  const loginEmail = useRef('');
+  const loginPassword = useRef('');
   const navegate = useNavigate();
 
   const postLogin = async () => {
+    if (loginEmail.current.value === '' || loginPassword.current.value === '') {
+      Toast.fire({
+        icon: 'error',
+        title: '帳號密碼不能為空',
+      });
+      return;
+    }
     const result = await firebaseUsers.signIn(
       loginEmail.current.value,
       loginPassword.current.value,
     );
+
     if (result.user) {
       Toast.fire({
         icon: 'success',
@@ -92,12 +132,25 @@ function Login() {
       navegate('/', { replace: true });
       loginEmail.current.value = '';
       loginPassword.current.value = '';
+    } else if (result === 'auth/wrong-password') {
+      Toast.fire({
+        icon: 'error',
+        title: '密碼輸入錯誤',
+      });
+    } else if (result === 'auth/user-not-found') {
+      Toast.fire({
+        icon: 'error',
+        title: '找不到使用者',
+      });
     } else {
-      setErrorMessage(result);
+      Toast.fire({
+        icon: 'error',
+        title: result,
+      });
     }
   };
   return (
-    <Wrapper>
+    <Wrapper onKeyUp={(e) => { if (e.key === 'Enter') { postLogin(); } }}>
       <LoginWrapper>
         <LeftBanner>
           <Link to="/">
@@ -113,21 +166,22 @@ function Login() {
           </p>
         </LeftBanner>
         <RightForm>
-          <h2>會員登入</h2>
-          <label htmlFor="loginEmail">
-            Email
-            <br />
-            <input type="email" name="loginEmail" placeholder="輸入email" ref={loginEmail} />
-          </label>
-          <label htmlFor="loginPassword">
-            密碼
-            <br />
-            <input type="password" name="loginPassword" placeholder="輸入密碼" ref={loginPassword} />
-          </label>
+          <h2>登入</h2>
+          <form>
+            <label htmlFor="loginEmail">
+              <span>Email</span>
+              <input type="email" name="loginEmail" placeholder="輸入email" ref={loginEmail} />
+            </label>
+            <label htmlFor="loginPassword">
+              <span>密碼</span>
+              <input type="password" autoComplete="off" name="loginPassword" placeholder="輸入密碼" ref={loginPassword} />
+            </label>
+          </form>
           <button type="button" onClick={postLogin}>登入</button>
-          {
-            errorMessage ? <p>{errorMessage}</p> : ''
-          }
+          <p>
+            還沒有註冊嗎?
+            <Link to="/?to=join">我要註冊</Link>
+          </p>
         </RightForm>
 
       </LoginWrapper>
